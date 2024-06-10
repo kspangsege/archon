dd346414a124a51818b188746c31a9d2d38c565f
Kristian Spangsege <kristian.spangsege@gmail.com>
2024-06-11 01:18:22 +0200

    ...

--- src/core_library.cmake
+++ src/core_library.cmake
@@ -28,7 +28,7 @@ add_library(Core
 
 set_target_properties(Core PROPERTIES OUTPUT_NAME "archon-core")
 
-if(MSVC)
+if(MSVC OR MINGW)
   # platform_info.cpp needs to link against GetFileVersionInfoSizeExW,
   # GetFileVersionInfoExW, and VerQueryValueW.
   target_link_libraries(Core version.lib)
