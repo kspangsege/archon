8d9eb17ecf5e9fd11f3de544dd2485f8d849f277
Kristian Spangsege <kristian.spangsege@gmail.com>
2024-06-10 21:46:26 +0200

    ...

--- src/archon/core/time.hpp
+++ src/archon/core/time.hpp
@@ -56,6 +56,8 @@ auto time_breakdown_utc(std::time_t) -> std::tm;
 using timespec_type = ::timespec; // Due to bug in LLVM libc++ (LLVM 10.0) on macOS
 #elif ARCHON_CYGWIN
 using timespec_type = ::timespec; // Cygwin bug
+#elif ARCHON_MINWG
+using timespec_type = ::timespec; // MinGW bug
 #else
 using timespec_type = std::timespec;
 #endif
