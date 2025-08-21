dc8057a51d7c1bf4c231edf974dfca103c478827
Kristian Spangsege <kristian.spangsege@gmail.com>
2025-08-21 13:09:56 +0200

    Introduce <archon/render/opengl.hpp>

--- src/archon/render/noinst/everything.hpp
+++ src/archon/render/noinst/everything.hpp
@@ -26,6 +26,7 @@
 
 
 #include <archon/render/render_namespace.hpp>
+#include <archon/render/opengl.hpp>
 #include <archon/render/impl/finite_sequence_memory.hpp>
 #include <archon/render/impl/finite_curve_memory.hpp>
 #include <archon/render/virt_trackball.hpp>
