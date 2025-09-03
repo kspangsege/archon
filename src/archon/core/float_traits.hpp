e6b90368363fab5b172250e28172fde5a9caab37
Kristian Spangsege <kristian.spangsege@gmail.com>
2025-09-03 19:12:31 +0200

    Add digits, min_exponent, and max_exponent to core::FloatTraits

--- src/archon/core/float_traits.hpp
+++ src/archon/core/float_traits.hpp
@@ -55,7 +55,7 @@ namespace archon::core {
 /// that.
 ///
 /// Below, A value `f`, that is representable in `F`, is *finite* if that value is in R (the
-/// real numbers). As such, a finite value cannot be inifinty, and cannot be NaN (not a
+/// real numbers). As such, a finite value cannot be infinity, and cannot be NaN (not a
 /// number).
 ///
 ///
@@ -123,16 +123,16 @@ namespace archon::core {
 ///     real numbers (R) rather than as the division that is offered by the C++
 ///     implementation.
 ///
-/// 11. If `i` is a postive integer, that is not representable in `F`, and `f` is the lowest
-///     representable finite floating-point value greater than `i`, or if `i` is a negative
-///     integer, that is not representable in `F`, and `f` is the highest representable
-///     finite floating-point value less than `i`, then `f / r` is an integer. Note that it
-///     follows from requirement 1 that `i` is greater than or equal to 2, or less than or
-///     equal to -2. Note also that it follows from requirement 7 that `f` itself is an
-///     integer, because if it was not, then `trunc(f)` would have been a representable
-///     value between `i` and `f`, or a representable value equal to `i`, both of which
-///     would be immediate contradictions. Note finally that it follows from requirement 10
-///     that `f / r` is represetnable in `F`.
+/// 11. If `i` is a positive integer, that is not representable in `F`, and `f` is the
+///     lowest representable finite floating-point value greater than `i`, or if `i` is a
+///     negative integer, that is not representable in `F`, and `f` is the highest
+///     representable finite floating-point value less than `i`, then `f / r` is an
+///     integer. Note that it follows from requirement 1 that `i` is greater than or equal
+///     to 2, or less than or equal to -2. Note also that it follows from requirement 7 that
+///     `f` itself is an integer, because if it was not, then `trunc(f)` would have been a
+///     representable value between `i` and `f`, or a representable value equal to `i`, both
+///     of which would be immediate contradictions. Note finally that it follows from
+///     requirement 10 that `f / r` is representable in `F`.
 ///
 /// Note that requirements 8 and 9 directly mirror the requirements for conversion between
 /// standard floating-point, and standard integer types in C++17.
@@ -140,7 +140,7 @@ namespace archon::core {
 /// A floating-point type that conforms to IEC 559 (IEEE 754) automatically satisfies all
 /// these requirements.
 ///
-/// FIXME: Probably also require direct non-throwing convertability to and from standard floating-point types (`float`, `double`, and `long double`).                            
+/// FIXME: Probably also require direct non-throwing convertibility to and from standard floating-point types (`float`, `double`, and `long double`).                            
 ///
 ///
 /// #### Requirements for valid specializations
@@ -149,8 +149,18 @@ namespace archon::core {
 /// requirements are met:
 ///
 /// - `T::radix` is the radix associated with `F`. It must be a compile-time constant of
-///   type `int`, and its value must be greater than, or equal to 2.  For the standard
+///   type `int`, and its value must be greater than, or equal to 2. For the standard
 ///   floating-point types, this is \c std::numeric_limits<F>::radix.
+///
+/// - `T::digits` is the number of radix-R digits in the mantissa, where R is `T::radix`. It
+///   must be a compile-time constant of type `int`, and its value must be greater than, or
+///   equal to 1. For the standard floating-point types, this is \c
+///   std::numeric_limits<F>::digits.
+///
+/// - `T::min_exponent` and `T::max_exponent` are the minimum and maximum values that the
+///   exponent can take on. They must be a compile-time constants of type `int`. For the
+///   standard floating-point types, these are \c std::numeric_limits<F>::min_exponent and
+///   \c std::numeric_limits<F>::max_exponent respectively.
 ///
 /// - `T::nextafter(from, to)` must be a valid non-throwing function invocation if `from`
 ///   and `to` are values of type `F`. The return type must be `F`. If `from` and `to` are
@@ -212,7 +222,10 @@ template<class F> struct FloatTraits<F, true> {
     static constexpr bool is_specialized = limits_type::is_iec559;
 #endif
 
-    static constexpr int radix = limits_type::radix;
+    static constexpr int radix        = limits_type::radix;
+    static constexpr int digits       = limits_type::digits;
+    static constexpr int min_exponent = limits_type::min_exponent;
+    static constexpr int max_exponent = limits_type::max_exponent;
 
     static auto nextafter(float_type from, float_type to) noexcept -> float_type
     {
